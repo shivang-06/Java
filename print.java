2fc2623068127291ba91ae73bd925721d43bb65c
Shivang Sharma <shivang.sharma15@outlook.com>
2020-02-18 10:23:35 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,14 @@
+ import java.util.*;
+          
+          public class print {
+          
+              public static void main(String[] args) {
+         	         // Write your code here
+                System.out.println("*****");
+                System.out.println("   * ");
+                System.out.println("  *  ");
+                System.out.println(" *   ");
+                System.out.println("*****");
+                  
+              }
+         }
