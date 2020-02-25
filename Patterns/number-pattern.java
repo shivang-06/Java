9122f3a9ba63adef62d5fba632bfbb7b1b7ef9e1
Shivang <shivang.sharma15@outlook.com>
2020-02-25 12:29:01 +0530

    number-pattern added.

---
+++ Patterns/number-pattern.java
@@ -0,0 +1,24 @@
+            // 1	
+            // 2	3	
+            // 4	5	6	
+            // 7	8	9	10	
+            // 11	12	13	14	15	
+
+            import java.util.*;
+     
+            public class Main{
+            
+            public static void main(String[] args) {
+                Scanner scn = new Scanner(System.in);
+               int n =scn.nextInt();
+               int count=1;
+                 for(int row = 1;row<=n;row++){
+                     for(int col=1;col<=row;col++){
+                         System.out.print(count+"\t");
+                         count++;
+                     }
+                     System.out.println();
+                 }
+            
+             }
+            }
