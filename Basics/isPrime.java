4cb1d1f1510af86ee8a6a04cb96d74b6918e0c84
Shivang <shivang.sharma15@outlook.com>
2020-04-01 18:06:26 +0530

    updated files

--- Basics/isPrime.java
+++ Basics/isPrime.java
@@ -1,5 +1,37 @@
 /**
- * isPrime
+ 
+Question
+ 1. You've to check whether a given number is prime or not.
+  2. Take a number "t" as input representing count of input numbers to be tested.
+  2. Take a number "n" as input "t" number of times.
+  3. For each input value of n, print "prime" if the number is prime and "not prime" otherwise.
+                               
+Input Format
+A number t
+  A number n
+  A number n
+  .. t number of times
+Output Format
+prime
+  not prime
+  not prime
+  .. t number of times
+Constraints
+1 <= t <= 10000
+  2 <= n < 10^9
+Sample Input
+5
+  19
+  21
+  33
+  37
+  121
+Sample Output
+prime
+  not prime
+  not prime
+  prime
+  not prime
  */
 import java.util.*;
 public class isPrime {
--- Basics/print.java
+++ Basics/print.java
@@ -1,4 +1,20 @@
- import java.util.*;
+/**
+ Question
+ 1. You are required to print a 'z' of size 5 using '*'.
+                               
+Input Format
+There is no input
+Output Format
+print-z-in-java
+
+Constraints
+No Constraints
+Sample Input
+There is no input
+Sample Output
+print-z-in-java
+*/
+import java.util.*;
           
           public class print {
           
