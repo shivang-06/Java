72d356d5f703374a6ecd1a1edb8045836c2e2077
Shivang <shivang.sharma15@outlook.com>
2020-04-01 18:09:57 +0530

    updated files

--- Basics/fibonacciNumber.java
+++ Basics/fibonacciNumber.java
@@ -1,3 +1,36 @@
+/*
+Question
+ 1. You've to print first n fibonacci numbers.
+  2. Take as input "n", the count of fibonacci numbers to print.
+  3. Print first n fibonacci numbers.
+                               
+Input Format
+n
+Output Format
+0
+  1
+  1
+  2
+  3
+  5
+  8
+  .. first n fibonaccis
+Constraints
+1 <= n < 40
+Sample Input
+10
+Sample Output
+0
+  1
+  1
+  2
+  3
+  5
+  8
+  13
+  31
+  34
+*/
 import java.util.*;
   
   public class Main{
