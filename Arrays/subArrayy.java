5e8b77e983d4890c8c3d37e977e37e3ca55fa647
Shivang <shivang.sharma15@outlook.com>
2020-04-11 21:16:32 +0530

    updated file

--- Arrays/subArrayy.java
+++ Arrays/subArrayy.java
@@ -1,3 +1,34 @@
+/*Question
+ 1. You are given a number n, representing the count of elements.
+2. You are given n numbers.
+3. You are required to print all subarrays of arr. Each subarray should be
+on separate line. For more clasrity check out sample input and output
+
+                               
+Input Format
+A number n
+n1
+n2
+.. n number of elements
+Output Format
+[Tab separated elements of subarray]
+..
+All subarrays
+Constraints
+1 <= n <= 10
+0 <= n1, n2, .. n elements <= 10 ^9
+Sample Input
+3
+10
+20
+30
+Sample Output
+10
+10  20
+10  20  30
+20
+20  30
+30*/
 import java.io.*;
 import java.util.*;
 
