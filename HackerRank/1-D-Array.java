242a87c215164eba52fc8a9b967fb305ba7aa59c
Shivang <shivang.sharma15@outlook.com>
2020-04-26 20:02:37 +0530

    new file added

---
+++ HackerRank/1-D-Array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for(int i =0;i<a.length;i++){
+            a[i] = scan.nextInt();
+        }
+
+        scan.close();
