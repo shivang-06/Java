02379fe490f5ed85a77f10192a2fcc183822d6d2
Shivang <shivang.sharma15@outlook.com>
2020-05-29 11:54:12 +0530

    new file added

--- Stack/MinimumStack.java
+++ Stack/MinimumStack.java
@@ -70,4 +70,98 @@ Sample Output
 10
 10
 10*/
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    public static class MinStack {
+        Stack < Integer > allData;
+        Stack < Integer > minData;
+
+        public MinStack() {
+            allData = new Stack < > ();
+            minData = new Stack < > ();
+        }
+
+
+        int size() {
+            // write your code here
+            return allData.size();
+        }
+
+        void push(int val) {
+            // write your code here
+            allData.push(val);
+            
+            if( minData.size()==0 || minData.peek()>=val ){
+                minData.push(val);
+            }
+        }
+
+        int pop() {
+            // write your code here
+            if (allData.size() == 0) {
+                System.out.println("Stack underflow");
+                return -1;
+            } else if (allData.peek() == minData.peek()) {
+                allData.pop();
+                int minVal = minData.pop();
+                return minVal;
+            } else {
+                int val = allData.pop();
+                return val;
+            }
+
+        }
+
+        int top() {
+            // write your code here
+            if (allData.size() == 0) {
+                System.out.println("Stack underflow");
+                return -1;
+            }
+            return allData.peek();
+        }
+
+        int min() {
+            // write your code here
+            if (minData.size() == 0) {
+                System.out.println("Stack underflow");
+                return -1;
+            }
+            return minData.peek();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        MinStack st = new MinStack();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("push")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                st.push(val);
+            } else if (str.startsWith("pop")) {
+                int val = st.pop();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("top")) {
+                int val = st.top();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(st.size());
+            } else if (str.startsWith("min")) {
+                int val = st.min();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            }
+            str = br.readLine();
+        }
+    }
+}
