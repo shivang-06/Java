ba0670f84b631eb61e2d7f1031f01d13ed6577f6
Shivang <shivang.sharma15@outlook.com>
2020-06-19 18:47:13 +0530

    added new file

---
+++ HashMaps/LongestConsecutiveSequenceofElements.java
@@ -0,0 +1,100 @@
+/*Question
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing elements of array a.
+3. You are required to print the longest sequence of consecutive elements in the array (ignoring duplicates)
+
+Note -> In case there are two sequences of equal length (and they are also the longest), then print the one for which the starting point of which occurs first in the array.
+Input Format
+A number n
+n1
+n2
+.. n number of elements
+Output Format
+Elements of longest sequence of consecutive elements of array in separate lines (ignoring duplicates)
+Constraints
+1 <= n <= 30
+0 <= n1, n2, .. n elements <= 15
+Sample Input
+17
+12
+5
+1
+2
+10
+2
+13
+7
+11
+8
+9
+11
+8
+9
+5
+6
+11
+Sample Output
+5
+6
+7
+8
+9
+10
+11
+12
+13*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap < Integer, Boolean > map = new HashMap < > ();
+        //initialization
+        for (int val: arr) {
+            map.put(val, true);
+        }
+
+        //find true starting point
+        for (int key: map.keySet()) {
+            if (map.containsKey(key - 1)) {
+                map.put(key, false); //value update
+            }
+        }
+
+        int sp = -1; //starting point
+        int size = 0;
+        //answer formation
+        for (int key: map.keySet()) {
+
+            if (map.get(key)) {
+                //local starting point found
+                int lsp = key; //local starting point
+                int lsize = 1;
+                int nextEle = key + 1;
+                while (map.containsKey(nextEle)) {
+                    lsize++;
+                    nextEle++;
+                }
+                if (lsize > size) {
+                    size = lsize;
+                    sp = lsp;
+                }
+            }
+        }
+
+        //print sequence
+        for (int i = 0; i < size; i++) {
+            System.out.println(sp + i);
+        }
+    }
+
+}
