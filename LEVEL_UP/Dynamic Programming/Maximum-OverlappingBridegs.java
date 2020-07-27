27eb42d840cd8af9fc04d96a1239eab8b207b982
Shivang <shivang.sharma15@outlook.com>
2020-07-28 04:01:20 +0530

    added maximum - non-overlapping bridges

--- LEVEL_UP/Dynamic Programming/Maximum-OverlappingBridegs.java
+++ LEVEL_UP/Dynamic Programming/Maximum-OverlappingBridegs.java
@@ -1,8 +1,7 @@
 /*Question
-  1. You are given a number n, representing the number of bridges on a river.
+1. You are given a number n, representing the number of bridges on a river.
 2. You are given n pair of numbers, representing the north bank and south bank co-ordinates of each bridge.
 3. You are required to print the count of maximum number of non-overlapping bridges.
-                               
                                
 Input Format
 A number n
@@ -13,69 +12,69 @@ Constraints
 0 <= n <= 20
 0 <= n1n, n1s, n2n, n2s, .. <= 100
 Sample Input
-10
-10 20
-2 7
-8 15
-17 3
-21 40
-50 4
-41 57
-60 80
-80 90
-1 30
-Sample Output
-7*/
+4
+43 26
+82 53
+94 23
+58 32
 
+
+Sample Output
+3*/
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    //Comparable interface is used to sort user defined datatype with help of compareTo class.
     static class pair implements Comparable < pair > {
-        int nb; //north bank
-        int sb; //south bank
+        int nb;
+        int sb;
+
         public int compareTo(pair other) {
-            if (this.nb != other.nb) {
+            if(this.nb!=other.nb){
                 return this.nb - other.nb;
-            } else {
-                return this.sb - other.sb;
+            }else{
+                return this.sb-other.sb;
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        pair[] bridges = new pair[n];
+        pair bridges[] = new pair[n];
         for (int i = 0; i < n; i++) {
             pair bridge = new pair();
             bridge.nb = sc.nextInt();
             bridge.sb = sc.nextInt();
-
+            
             bridges[i] = bridge;
         }
         Arrays.sort(bridges);
+
         int[] dp = new int[n];
 
+        // applying longest increasing subsequnce on the basis of south banks of bridges
         for (int i = 0; i < n; i++) {
             int max = 0;
             
             for (int j = 0; j < i; j++) {
-                if (bridges[i].sb > bridges[j].sb && max < dp[j]) {
+                if ((bridges[j].sb < bridges[i].sb) && (max < dp[j])) {
                     max = dp[j];
                 }
             }
             dp[i] = max + 1;
+            
         }
 
-        int lis = dp[0];
+        int ans = dp[0];
         for (int i = 1; i < n; i++) {
-            if (lis < dp[i]) {
-                lis = dp[i];
+            if (dp[i] > ans) {
+                ans = dp[i];
             }
         }
-        System.out.println(lis);
+        System.out.println(ans);
     }
 
 }
