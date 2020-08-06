b3bca1d00e8e0c0ac2f361f103f84e814481a47d
Shivang <shivang.sharma15@outlook.com>
2020-08-06 14:17:47 +0530

    updated mazePaths with jumps

--- LEVEL_UP/Recursion_LU/mazePathWithJumps.java
+++ LEVEL_UP/Recursion_LU/mazePathWithJumps.java
@@ -1,28 +1,30 @@
-public class mazePathWithJumps{
-    public static void main(String[] args) {
-        System.out.println(solve(0,0,2,2,""));
+public class mazePathWithJumps {
+
+  public static void main(String[] args) {
+    System.out.println(solve(0, 0, 2, 2, ""));
+  }
+
+  public static int solve(int sr, int sc, int dr, int dc, String psf) {
+    if (sr > dr || sc > dc) {
+      return 0;
     }
-    public static int solve(int sr, int sc, int dr, int dc, String psf){
-        if(sr > dr || sc > dc){
-            return 0;
-        }
-        if(sr == dr && sc == dc){
-            System.out.println(psf);
-            return 1;
-        }
-        int count =0;
-        //horizontal
-        for(int jump = 1;jump <= dc;jump++){
-            count +=solve(sr,sc+jump,dr,dc,psf+"H"+jump);
-        }
-        //vertical
-        for(int jump = 1;jump <= dr;jump++){
-            count+=solve(sr+jump,sc,dr,dc,psf+"V"+jump);
-        }
-        
-        for(int jump = 1;jump <= dr&&jump <= dc;jump++){
-            count+=solve(sr+jump,sc+jump,dr,dc,psf+"D"+jump);
-        }
-        return count;
+    if (sr == dr && sc == dc) {
+      System.out.println(psf);
+      return 1;
     }
+    int count = 0;
+    //horizontal
+    for (int jump = 1; sc + jump <= dc; jump++) {
+      count += solve(sr, sc + jump, dr, dc, psf + "H" + jump);
+    }
+    //vertical
+    for (int jump = 1; sr + jump <= dr; jump++) {
+      count += solve(sr + jump, sc, dr, dc, psf + "V" + jump);
+    }
+
+    for (int jump = 1; sr + jump <= dr && sc + jump <= dc; jump++) {
+      count += solve(sr + jump, sc + jump, dr, dc, psf + "D" + jump);
+    }
+    return count;
+  }
 }
