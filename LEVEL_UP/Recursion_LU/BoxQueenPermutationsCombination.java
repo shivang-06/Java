5d3b65c833e5385b19c78d1a046c40c4d50394d8
Shivang <shivang.sharma15@outlook.com>
2020-08-08 20:48:51 +0530

    updated

---
+++ LEVEL_UP/Recursion_LU/BoxQueenPermutationsCombination.java
@@ -0,0 +1,18 @@
+public class BoxQueenPermutationsCombination{
+    public static void main(String[] args) {
+        
+    }
+    public static int BoxQueenPermu(int[] q,int[] space){
+        if()
+    }
+    public static int BoxQueenCombi(int[] q, int[] space,int sidx) {
+        
+        
+        BoxQueenCombi(q,space,sidx+1);
+        BoxQueenCombi(q,space,sidx);
+    }
+
+    public static coinChangeInfinitePermutations(int[] arr,int idx,int tar){
+        if()
+    }
+}
--- LEVEL_UP/Recursion_LU/NQueenPermutationsCombination.java
+++
@@ -1,8 +0,0 @@
-public class NQueenPermutationsCombination{
-    public static void main(String[] args) {
-        
-    }
-    public static int NQueenPermu(int[] q,int[] space){
-        
-    }
-}
